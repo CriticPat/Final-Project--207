6f0c34fe94cfa1ef307b241a8f726ddba66ffe83
CriticPat <151105228+CriticPat@users.noreply.github.com>
2024-11-18 23:17:50 -0600

    Update Final Project- Source Code.cpp

--- Final Project- Source Code.cpp
+++ Final Project- Source Code.cpp
@@ -1,3 +1,25 @@
 //Patrick Acuña & Allyn Novelo 
 #include <iostream>
+#include <cstring>
 using namespace std;
+//Base class containing Employee methods and attributes
+class Employee{
+    protected:
+        char firstname[15];
+        char lastname[15];
+        int employeeID;
+        string department [10] = {"Humanity", "Business", "IT", "Maintenance"};
+        float baseSalary;
+    public:
+
+};
+//Inherited Class containing its attributes and methods/attributes from Employee Class. 
+class SalariedEmployee: public Employee{
+    private:
+        float monthlysalary; 
+        float annualsalary;
+    public:
+};
+int main(){
+    return 0; 
+}
